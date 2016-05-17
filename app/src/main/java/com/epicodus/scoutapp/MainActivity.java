7bdc230b731d5d7eb076595f895b9db2f5ce11dc
Jon <jonsweeney26@gmail.com>
2016-05-17 16:47:23 -0700

    get user name

--- app/src/main/java/com/epicodus/scoutapp/MainActivity.java
+++ app/src/main/java/com/epicodus/scoutapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.epicodus.scoutapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Button mLoginMainButton;
+    private EditText mUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUserName = (EditText) findViewById(R.id.userName);
+        mLoginMainButton = (Button) findViewById(R.id.logInMainButton);
+        mLoginMainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String location = mUserName.getText().toString();
+                Log.d(TAG, location);
+                Intent intent = new Intent(MainActivity.this, HomePageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
